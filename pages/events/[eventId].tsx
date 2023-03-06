c4fc91af99a721dad7411de37befc88f33c83f94
Victor Hristov <victor.hristov@mentormate.com>
2023-03-06 13:22:24 +0200

    Add event details components and styles

--- pages/events/[eventId].tsx
+++ pages/events/[eventId].tsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { useRouter } from "next/router";
+import { getEventById } from "@/dummy-data";
+import EventSummary from "@/components/event-detail/EventSummary.component";
+import EventContent from "@/components/event-detail/EventContent.component";
+import EventLogistics from "@/components/event-detail/EventLogistics.component";
 
 const EventDetailsPage = () => {
-  return <div>EventDetailsPage</div>;
+  const router = useRouter();
+  const event = getEventById(router.query.eventId);
+
+  if (!event) {
+    return <p>No Event Found</p>;
+  }
+
+  return (
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
+  );
 };
 
 export default EventDetailsPage;
